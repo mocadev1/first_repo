8f17fc67397f13a7ae14ab9bdaa391094db443b0
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2020-09-20 20:08:40 -0500

    An upgrade that returns the set of vowels in the phrase

--- vsearch.py
+++ vsearch.py
@@ -1,6 +1,6 @@
-def search4vowels(word):
-    """Display any vowels found in a supplied word."""
+"""We are stating that the "phrase" argument is expected to be a string
+   and that the function return a set to its caller"""
+def search4vowels(phrase:str) -> set:
+    """Return any vowels found in a supplied word."""
     vowels = set('aeiou')
-    found = vowels.intersection(set(word))
-    for vowel in found:
-    	print(vowel)
+    return vowels.intersection(set(phrase))
