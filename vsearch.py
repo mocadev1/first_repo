cc1bbb99e66cd5e24852fd51cce9dcabd87c6d9d
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2020-09-20 20:16:12 -0500

    Another function that returns a set of letters found in a phrase

--- vsearch.py
+++ vsearch.py
@@ -1,6 +1,10 @@
 """We are stating that the "phrase" argument is expected to be a string
    and that the function return a set to its caller"""
 def search4vowels(phrase:str) -> set:
-    """Return any vowels found in a supplied word."""
+    """Return any vowels found in a supplied phrase."""
     vowels = set('aeiou')
     return vowels.intersection(set(phrase))
+
+def search4letters(phrase:str, letters:str='aeiou') -> set:
+    """Return a set of the 'letters' found in 'phrase'"""
+    return set(letters).intersection(set(phrase))
