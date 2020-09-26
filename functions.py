8cbab27a99d50ee0e9bffa9596d1d0cbb1643649
Omar Beltran <carlos.beltran2959@alumnos.udg.mx>
2020-09-26 07:54:33 -0500

    Implemented add() and substraction()

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 def add(a, b : float) -> float:
-    
-
+    """ Returns the addition of two numbers """
+    return float(a + b)
 
 def substraction(a, b: float) -> float:
-    
+    """ Returns the substraction of two numbers """
+    return float (a - b)
